5f53af63699d87c1d59e4a11017afbf2fa5773ac
pndao <pndao@inbox.ru>
2023-06-21 11:00:46 -0400

    ok

---
+++ network_scanner.py
@@ -0,0 +1,21 @@
+#!/ussr/bin/env python
+
+import scapy.all as scapy
+
+
+def scan(ip):
+    apr_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/apr_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print('IP\t\t\tMAC Address\n---------------------------------------------')
+    clients_list = []
+    for element in answered_list:
+        client_dict = {'ip': element[1].psrc, 'mac':  element[1].hwsrc}
+        clients_list.append(client_dict)
+        print(element[1].psrc + '\t\t' + element[1].hwsrc)
+    print(clients_list)
+
+
+scan('10.0.2.2/24')
